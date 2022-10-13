0b1bff87d004665c8669063d16e3de4b12378a81
İlker Karaca <ikaracadeveloper@gmail.com>
2022-10-13 12:54:55 +0300

    Added methods of Weapons class

--- src/Game.java
+++ src/Game.java
@@ -10,5 +10,27 @@ public class Game {
         System.out.println(player.getName() + " Hoşgeldiniz!");
         System.out.println("Lütfen bir karakter seçin: ");
         player.selectChar();
+        Location location = null;
+        while (true){
+            System.out.println("Bölgeler");
+            System.out.println("1 - Güvenli Ev");
+            System.out.println("2 - Mağaza");
+            System.out.println("Lütfen gitmek istediğiniz bölgeyi seçin : ");
+            int selectLoc = input.nextInt();
+            switch (selectLoc){
+                case 1:
+                    location = new SafeHouse(player);
+                    break;
+                case 2:
+                    location = new ToolStore(player);
+                    break;
+                default:
+                    System.out.println("Hata");
+            }
+            if (!location.onLocation()){
+                System.out.println("GAME OVER!");
+                break;
+            }
+        }
     }
 }
--- src/ToolStore.java
+++ src/ToolStore.java
@@ -7,6 +7,34 @@ public class ToolStore extends NormalLoc{
     @Override
     public boolean onLocation() {
         System.out.println("Mağazaya Hoşgeldiniz!");
+        System.out.println("1 - Silahlar");
+        System.out.println("2 - Zırhlar");
+        System.out.println("3 - Çıkış Yap");
+        System.out.print("Seçiminiz : ");
+        int selectCase = input.nextInt();
+        while (selectCase < 1 && selectCase > 3){
+            System.out.print("Geçersiz bir seçim yaptınız!\n Lütfen yeni bir seçim yapınız : ");
+            selectCase = input.nextInt();
+        }
+        switch (selectCase){
+            case 1:
+                printWeapons();
+                break;
+            case 2:
+                printArmors();
+                break;
+            case 3:
+                return true;
+        }
         return true;
+    }
+    public void printWeapons(){
+        System.out.println("-------- Silahlar --------");
+        for (Weapon w : Weapon.weapons()) {
+            System.out.println("Id : \t" +w.getId() + "\tSilah Adı : " + w.getName() + "\tHasar : " + w.getDamage() + "\tFiyat : " + w.getPrice());
+        }
+    }
+    public void printArmors(){
+        System.out.println("Silahlar");
     }
 }
